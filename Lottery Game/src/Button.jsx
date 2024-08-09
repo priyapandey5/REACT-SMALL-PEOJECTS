2739ef3879b936ae7e4cc0db0eac47be456e5f9a
Priya Pandey <129404384+priyapandey5@users.noreply.github.com>
2024-08-09 20:01:34 +0530

    Delete Lottery Game/src directory

--- Lottery Game/src/Button.jsx
+++
@@ -1,3 +0,0 @@
-export default function Button({action}) {
-    return <button onClick={action}>Buy New Ticket</button>
-}
--- Lottery Game/src/TicketNum.jsx
+++
@@ -1,7 +0,0 @@
-import "./TicketNum.css";
-
-export default function TicketNum({num}) {
-    return (
-        <span className="TicketNum">{num}</span>
-    );
-}
